6a00ac0752ad26d1de45794a8d14edcae8a98d26
David Miguel <me@davidmiguel.com>
2023-07-13 18:45:54 +0200

    PR improvements

--- packages/langchain/test/documents/loaders/json.dart
+++ packages/langchain/test/documents/loaders/json.dart
@@ -4,16 +4,15 @@ import 'package:test/test.dart';
 void main() {
   group('JsonLoader tests', () {
     test('Test that a json file can be loaded', () async {
-      const filePath = './test/documents/loaders/assets/example.json';
+      const filePath = './test/documents/loaders/assets/example_1.json';
 
       final expectedDoc = Document(
         pageContent: 'Foo\nBar\nBaz\n',
         metadata: {
-          'source': '$filePath/example.json',
           'path': filePath,
-          'name': 'example.json',
-          'size': 30,
-          'lastModified': DateTime.parse('2023-07-11 17:54:43.000'),
+          'name': 'example_1.json',
+          'size': 32,
+          'lastModified': DateTime.parse('2023-07-13 17:49:35.000'),
         },
       );
 
@@ -27,6 +26,78 @@ void main() {
         emitsInOrder([
           expectedDoc,
           emitsDone,
+        ]),
+      );
+    });
+
+    test('test extracting numeric field', () async {
+      const filePath = './test/documents/loaders/assets/example_2.json';
+
+      const loader = JsonLoader(
+        filePath: filePath,
+        jpSchema: r'$..price',
+      );
+
+      expect(
+        loader.lazyLoad(),
+        emitsInOrder([
+          (final Document doc) => doc.pageContent == '8.95',
+          (final Document doc) => doc.pageContent == '12.99',
+          (final Document doc) => doc.pageContent == '8.99',
+          (final Document doc) => doc.pageContent == '22.99',
+          (final Document doc) => doc.pageContent == '19.95',
+        ]),
+      );
+    });
+
+    test('test extracting string field', () async {
+      const filePath = './test/documents/loaders/assets/example_2.json';
+
+      const loader = JsonLoader(
+        filePath: filePath,
+        jpSchema: r'$..category',
+      );
+
+      expect(
+        loader.lazyLoad(),
+        emitsInOrder([
+          (final Document doc) => doc.pageContent == 'reference',
+          (final Document doc) => doc.pageContent == 'fiction',
+          (final Document doc) => doc.pageContent == 'fiction',
+          (final Document doc) => doc.pageContent == 'fiction',
+        ]),
+      );
+    });
+
+    test('test invalid jpSchema throws exception', () async {
+      const filePath = './test/documents/loaders/assets/example_2.json';
+
+      const loader = JsonLoader(
+        filePath: filePath,
+        jpSchema: r'.[]',
+      );
+
+      expect(() async => loader.load(), throwsException);
+    });
+
+    test('test custom metadata', () async {
+      const filePath = './test/documents/loaders/assets/example_2.json';
+
+      final loader = JsonLoader(
+        filePath: filePath,
+        jpSchema: r'$..category',
+        metadataBuilder: (final matchValue, final fileMetadata) {
+          return {'length': matchValue.toString().length};
+        },
+      );
+
+      expect(
+        loader.lazyLoad(),
+        emitsInOrder([
+          (final Document doc) => doc.metadata['length'] == 9,
+          (final Document doc) => doc.metadata['length'] == 7,
+          (final Document doc) => doc.metadata['length'] == 7,
+          (final Document doc) => doc.metadata['length'] == 7,
         ]),
       );
     });
