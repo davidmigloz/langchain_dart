c02fdffa131cef79bd9bbe0bd4c32d54941e6214
David Miguel <me@davidmiguel.com>
2023-08-01 13:17:44 +0200

    CsvLoader improvements

--- packages/langchain/lib/src/documents/loaders/csv.dart
+++ packages/langchain/lib/src/documents/loaders/csv.dart
@@ -1,177 +1,182 @@
 import 'dart:convert';
-import 'dart:io';
 
+import 'package:cross_file/cross_file.dart';
 import 'package:csv/csv.dart';
 
 import '../models/models.dart';
 import 'base.dart';
 
+/// {@template csv_loader}
 /// A document loader for loading documents from CSV files.
 ///
-/// This class extends the [BaseDocumentLoader] and is used to load documents from
-/// CSV files. It reads the CSV file specified by the `filePath` and transforms each
-/// row into a [Document] object, which represents a single document with metadata
-/// and page content.
+/// It reads the CSV file specified by [filePath] and transforms each row into
+/// a [Document] object.
+///
+/// You can customize the [fields] that you want to extract or rename them
+/// using [fieldsOverride]. The field, text, and line delimiters can also be
+/// customized using [fieldDelimiter], [fieldTextDelimiter], [fieldTextEndDelimiter],
+/// and [eol].
+///
+/// The fields are added to the page content in the following format:
+/// ```
+/// {field1Name}: {field1Value}
+/// {field2Name}: {field2Value}
+/// ...
+/// {fieldNName}: {fieldNValue}
+/// ```
+///
+/// You can specify a [sourceField] to use as the source metadata field for
+/// each document. Otherwise, the [filePath] will be used as source.
+///
+/// Example:
+/// ```dart
+/// final loader = CsvLoader('path/to/file.csv');
+/// final documents = await loader.load();
+/// ```
+/// {@endtemplate}
 class CsvLoader extends BaseDocumentLoader {
-  /// Creates a new instance of [CsvLoader].
-  ///
-  /// The `filePath` parameter is required and represents the file path of the CSV file
-  /// to be loaded. The `delimiter`, `quoteChar`, and `eol` parameters are optional and
-  /// can be customized to match the CSV file's formatting. The `fieldNames` parameter allows
-  /// specifying the column names in the CSV file, and `metadataBuilder` allows building custom
-  /// metadata for each document. If not provided, the default metadata is used.
+  /// {@macro csv_loader}
   CsvLoader(
     this.filePath, {
-    this.sourceColumn,
-    this.useCols,
-    this.delimiter = ',',
-    this.quoteChar = '"',
+    this.fields,
+    this.fieldsOverride,
+    this.fieldDelimiter = ',',
+    this.fieldTextDelimiter = '"',
+    this.fieldTextEndDelimiter,
     this.eol = '\n',
-    this.fieldNames,
-    // this.metadataBuilder,
+    this.sourceField,
+    this.metadataBuilder,
   });
 
   /// The file path of the CSV file to be loaded.
-  ///
-  /// This property holds the file path of the CSV file from which the documents
-  /// will be loaded. The loader reads the CSV file and processes each row to
-  /// create [Document] objects.
   final String filePath;
 
-  /// Use the sourceColumn argument to specify a source for the document created from each row. Otherwise file_path will be used as the source for all documents created from the CSV file.
+  /// Optional field to specify which fields from the row to extract and add to
+  /// the page content of the document.
   ///
-  /// This is useful when using documents loaded from CSV files for chains that answer questions using sources.
-  final String? sourceColumn;
+  /// If not provided, all row fields are extracted.
+  /// ```
+  final List<String>? fields;
 
-  /// Use the useCols argument to specify which columns to use from the CSV file.
-  final List<String>? useCols;
+  /// Optional field to override the field names from the CSV file.
+  ///
+  /// If not provided, the column names from the CSV file are used.
+  List<String>? fieldsOverride;
 
   /// The delimiter used to separate fields in the CSV file.
   ///
-  /// This property specifies the delimiter used in the CSV file to separate fields.
-  /// By default, it is set to a comma (`,`), which is the most commonly used delimiter
-  /// for CSV files. You can change this value to match the delimiter used in your CSV file.
-  final String delimiter;
+  /// By default, it is set to comma (`,`).
+  final String fieldDelimiter;
 
-  /// The character used to quote fields containing special characters.
+  /// The delimiter which (optionally) surrounds text / fields.
   ///
-  /// This property holds the quote character used to enclose fields containing special
-  /// characters like the delimiter itself. By default, it is set to double quotation marks
-  /// (`"`), which is the standard quote character for CSV files. You can change this value
-  /// to match the quote character used in your CSV file.
-  final String quoteChar;
+  /// By default, it is set to double quotation marks (`"`).
+  final String fieldTextDelimiter;
 
-  /// The end-of-line character used to separate rows in the CSV file.
+  /// Optional end delimiter for text. This allows text to be quoted with
+  /// different start / end delimiters: Example:  «abc».
   ///
-  /// This property specifies the end-of-line (EOL) character used to separate rows in the CSV
-  /// file. By default, it is set to the newline character (`\n`), which is commonly used as
-  /// the EOL character in CSV files. You can change this value to match the EOL character
-  /// used in your CSV file.
+  /// If null, [fieldTextDelimiter] is used as the end delimiter.
+  final String? fieldTextEndDelimiter;
+
+  /// The end-of-line character used to separate rows in the CSV file. The eol
+  /// is optional for the last row.
+  ///
+  /// By default, it is set to the newline character (`\n`).
   final String eol;
 
-  /// The list of field names for the CSV file.
+  /// Optional field to specify a source for the document created from each
+  /// row. Otherwise [filePath] will be used as the source for all documents
+  /// created from the CSV file.
   ///
-  /// This property holds the list of field names that define the columns in the CSV file.
-  /// If provided, it is used to create a space-separated `pageContent` for each document,
-  /// combining the values of the fields specified in `fieldNames`. If not provided, each
-  /// row in the CSV file is treated as a separate `pageContent`.
-  List<String>? fieldNames;
+  /// This is useful when using documents loaded from CSV files for chains that
+  /// answer questions using sources.
+  final String? sourceField;
 
-  /// A function to build custom metadata for each document.
+  /// An optional function to build custom metadata for each document based on
+  /// the row content and the default metadata.
   ///
-  /// This property holds a function that allows building custom metadata for each document
-  /// based on the CSV row data and the metadata base. The function takes two parameters:
-  /// `line`, which is a map containing the field names and data for the CSV row, and
-  /// `fileMetadata`, which is a map containing base metadata for all documents in the CSV file.
-  /// If not provided, the default metadata is used, including the file path, file name, file size,
-  /// and last modified date.
-  // final Map<String, dynamic> Function(
-  //   Map<String, dynamic> line,
-  //   Map<String, dynamic> fileMetadata,
-  // )? metadataBuilder;
+  /// If null, the default row metadata will be used.
+  final Map<String, dynamic> Function(
+    Map<String, dynamic> row,
+    Map<String, dynamic> rowMetadata,
+  )? metadataBuilder;
 
-  /// Loads documents from the CSV file and returns a lazy-loading stream.
-  ///
-  /// This method overrides the `lazyLoad` method of the [BaseDocumentLoader] class. It reads
-  /// the CSV file specified in `filePath`, processes each row, and yields a stream of [Document]
-  /// objects representing the documents in the CSV file. Each document contains the metadata and
-  /// page content based on the `fieldNames` and `metadataBuilder`, if provided.
   @override
   Stream<Document> lazyLoad() async* {
-    final file = File(filePath);
+    final file = XFile(filePath);
 
-    final csvLines = await file
-        .openRead()
-        .transform(utf8.decoder)
-        .transform(
-          CsvToListConverter(
-            fieldDelimiter: delimiter,
-            textDelimiter: quoteChar,
-            eol: eol,
-          ),
-        )
-        .toList();
+    final csvLinesStream =
+        file.openRead().cast<List<int>>().transform(utf8.decoder).transform(
+              CsvToListConverter(
+                fieldDelimiter: fieldDelimiter,
+                textDelimiter: fieldTextDelimiter,
+                textEndDelimiter: fieldTextEndDelimiter,
+                eol: eol,
+              ),
+            );
 
-    var firstRow = true;
-
-    final fieldNameAndPosition = <String, int>{};
-
-    for (final line in csvLines) {
-      if (firstRow) {
-        final overrideFieldNames = line.length == fieldNames?.length;
-
-        final overrideCols = useCols != null && useCols!.isNotEmpty;
-
-        if (fieldNames != null && !overrideFieldNames) {
-          throw Exception(
-            'The length of the line in the csv file is not equal to the length of the fieldNames.',
-          );
-        }
-
-        // Extract field names and their positions
-        for (var i = 0; i < line.length; i++) {
-          final fieldName =
-              overrideFieldNames ? fieldNames![i] : line[i] as String;
-
-          if (!overrideCols) {
-            fieldNameAndPosition[fieldName] = i;
-
-            continue;
-          }
-
-          if (useCols!.contains(line[i] as String)) {
-            fieldNameAndPosition[fieldName] = i;
-          }
-        }
-
-        if (useCols != null && useCols!.length != fieldNameAndPosition.length) {
-          throw Exception(
-            'Not all columns listed in useCols were found in the csv file.',
-          );
-        }
-
-        firstRow = false;
-      } else {
-        final metadata = <String, dynamic>{
-          'source': filePath,
-        };
-
-        if (sourceColumn != null &&
-            fieldNameAndPosition[sourceColumn!] != null) {
-          metadata['source'] = line[fieldNameAndPosition[sourceColumn!]!];
-        }
-
-        final doc = Document(
-          pageContent: fieldNameAndPosition.keys
-              .map(
-                (final name) => '$name: ${line[fieldNameAndPosition[name]!]}',
-              )
-              .join('\n'),
-          metadata: metadata,
+    final fieldsToPositions = <String, int>{};
+    final pageContentFields = <String>[];
+    await for (final row in csvLinesStream) {
+      // Process CSV header
+      if (fieldsToPositions.isEmpty) {
+        assert(row.isNotEmpty, 'Header row cannot be empty');
+        assert(
+          fields == null &&
+                  (fieldsOverride == null ||
+                      fieldsOverride!.length == row.length) ||
+              fields != null &&
+                  (fieldsOverride == null ||
+                      fieldsOverride!.length == fields!.length),
+          'You have specified more field names than fields',
         );
 
-        yield doc;
+        for (int i = 0; i < row.length; i++) {
+          final field = row[i] as String;
+          if (field == sourceField || (fields?.contains(field) ?? true)) {
+            final fieldName =
+                fieldsOverride?[fieldsToPositions.length] ?? field;
+            fieldsToPositions[fieldName] = i;
+          }
+        }
+
+        assert(
+          fields == null ||
+              (fieldsOverride ?? fields)!.every(
+                (final field) => fieldsToPositions[field] != null,
+              ),
+          'You have specified a field that does not exist in your csv file.',
+        );
+
+        pageContentFields.addAll(
+          fields == null || sourceField == null || fields!.contains(sourceField)
+              ? fieldsToPositions.keys
+              : fields!.where((final field) => field != sourceField),
+        );
+        continue;
       }
+
+      // Process rows
+      final rowMetadata = <String, dynamic>{
+        if (sourceField == null)
+          'source': filePath
+        else
+          'source': row[fieldsToPositions[sourceField!]!],
+      };
+      final rowContent = {
+        for (final field in pageContentFields)
+          field: row[fieldsToPositions[field]!],
+      };
+
+      final doc = Document(
+        pageContent: rowContent.entries
+            .map((final field) => '${field.key}: ${field.value}')
+            .join('\n'),
+        metadata: metadataBuilder?.call(rowContent, rowMetadata) ?? rowMetadata,
+      );
+
+      yield doc;
     }
   }
 }
