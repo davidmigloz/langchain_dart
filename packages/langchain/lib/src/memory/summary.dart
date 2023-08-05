f631d9e529d99319afe671b5aff441436e43ea31
Maurice Poirrier <mauricepoirrier@gmail.com>
2023-08-05 09:56:49 -0400

    feat(memory): Add ConversationSummaryMemory (#27)
    Co-authored-by: David Miguel <me@davidmiguel.com>

---
+++ packages/langchain/lib/src/memory/summary.dart
@@ -0,0 +1,227 @@
+import '../chains/chains.dart';
+import '../model_io/chat_models/models/models.dart';
+import '../model_io/chat_models/utils.dart';
+import '../model_io/language_models/language_models.dart';
+import '../model_io/prompts/prompts.dart';
+import 'base.dart';
+import 'chat.dart';
+import 'models/models.dart';
+import 'stores/message/history.dart';
+import 'stores/message/in_memory.dart';
+
+const _template = '''
+Progressively summarize the lines of conversation provided, adding onto the previous summary returning a new summary.
+
+EXAMPLE
+Current summary:
+The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good.
+
+New lines of conversation:
+Human: Why do you think artificial intelligence is a force for good?
+AI: Because artificial intelligence will help humans reach their full potential.
+
+New summary:
+The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good because it will help humans reach their full potential.
+END OF EXAMPLE
+
+Current summary:
+{summary}
+
+New lines of conversation:
+{new_lines}
+
+New summary:''';
+
+const _promptTemplate = PromptTemplate(
+  template: _template,
+  inputVariables: {'summary', 'new_lines'},
+);
+
+/// {@template conversation_summary_memory}
+/// Memory that summarizes a conversation over time. This is useful for longer
+/// conversations where keeping the full message history would take up too many
+/// tokens.
+///
+/// It requires an [llm] to summarize the conversation. You can customize the
+/// summarization prompt using [summaryPromptTemplate].
+///
+/// Every time [saveContext] is called, it will generate a new summary of the
+/// conversation using the previous summary and the new messages.
+///
+/// It uses [ChatMessageHistory] as in-memory storage by default.
+///
+/// You can provide an initial summary using [initialSummary]. If you have a
+/// [chatHistory] with previous messages, you can use the factory constructor
+/// [ConversationSummaryMemory.fromChatHistory] to generate an initial summary.
+///
+/// Example:
+/// ```dart
+/// final memory = ConversationSummaryMemory(llm: OpenAI(apiKey: '...'));
+/// await memory.saveContext({'foo': 'bar'}, {'bar': 'foo'});
+/// final res = await memory.loadMemoryVariables();
+/// // {'history': 'System: Human said bar'}
+/// ```
+/// {@endtemplate}
+final class ConversationSummaryMemory<LLMType extends BaseLanguageModel>
+    extends BaseChatMemory {
+  /// {@macro conversation_summary_memory}
+  ConversationSummaryMemory({
+    super.chatHistory,
+    super.inputKey,
+    super.outputKey,
+    super.returnMessages = false,
+    required this.llm,
+    final String initialSummary = '',
+    this.summaryPromptTemplate = _promptTemplate,
+    this.summaryMessageBuilder,
+    this.memoryKey = BaseMemory.defaultMemoryKey,
+    this.systemPrefix = SystemChatMessage.defaultPrefix,
+    this.humanPrefix = HumanChatMessage.defaultPrefix,
+    this.aiPrefix = AIChatMessage.defaultPrefix,
+    this.functionPrefix = FunctionChatMessage.defaultPrefix,
+  }) : _buffer = initialSummary;
+
+  /// Language model to use for summarizing the conversation.
+  final LLMType llm;
+
+  /// [PromptTemplate] to use for summarizing previous conversations.
+  /// This currently expect two input variables: `summary` and `new_lines`.
+  /// summary is the previous summary and new_lines are the new messages
+  /// formatted to add.
+  final BasePromptTemplate summaryPromptTemplate;
+
+  /// A builder to construct the chat message that contains the summary.
+  /// If null, it will use [ChatMessage.system].
+  final ChatMessage Function(String summary)? summaryMessageBuilder;
+
+  /// The memory key to use for the chat history.
+  /// This will be passed as input variable to the prompt.
+  final String memoryKey;
+
+  /// The prefix to use for system messages.
+  final String systemPrefix;
+
+  /// The prefix to use for human messages.
+  final String humanPrefix;
+
+  /// The prefix to use for AI messages.
+  final String aiPrefix;
+
+  /// The prefix to use for function messages.
+  final String functionPrefix;
+
+  /// Store the summarized chat history in memory.
+  /// This does not concatenate, changes every new [ChatMessage] are added.
+  String _buffer;
+
+  @override
+  Set<String> get memoryKeys => {memoryKey};
+
+  /// Instantiate a [ConversationSummaryMemory] from a list of [ChatMessage]s.
+  /// Use this factory method if you want to generate a [ConversationSummaryMemory]
+  /// with pre-loaded history and don't have the summary of the messages.
+  /// Required a [BaseLanguageModel] to use for summarizing.
+  static Future<ConversationSummaryMemory> fromMessages({
+    required final BaseLanguageModel llm,
+    required final BaseChatMessageHistory chatHistory,
+    final String? inputKey,
+    final String? outputKey,
+    final bool returnMessages = false,
+    final String initialSummary = '',
+    final PromptTemplate summaryPromptTemplate = _promptTemplate,
+    final ChatMessage Function(String summary)? summaryMessageBuilder,
+    final String memoryKey = BaseMemory.defaultMemoryKey,
+    final String systemPrefix = SystemChatMessage.defaultPrefix,
+    final String humanPrefix = HumanChatMessage.defaultPrefix,
+    final String aiPrefix = AIChatMessage.defaultPrefix,
+    final String functionPrefix = FunctionChatMessage.defaultPrefix,
+    final int summaryStep = 2,
+  }) async {
+    final memory = ConversationSummaryMemory(
+      chatHistory: chatHistory,
+      llm: llm,
+      inputKey: inputKey,
+      outputKey: outputKey,
+      returnMessages: returnMessages,
+      initialSummary: initialSummary,
+      summaryPromptTemplate: summaryPromptTemplate,
+      summaryMessageBuilder: summaryMessageBuilder,
+      memoryKey: memoryKey,
+      systemPrefix: systemPrefix,
+      humanPrefix: humanPrefix,
+      aiPrefix: aiPrefix,
+      functionPrefix: functionPrefix,
+    );
+    final messages = await chatHistory.getChatMessages();
+    for (var i = 0; i < messages.length; i += summaryStep) {
+      final summary = await memory._summarize(
+        messages.sublist(i, i + summaryStep),
+        memory._buffer,
+      );
+      memory._buffer = summary;
+    }
+    return memory;
+  }
+
+  @override
+  Future<MemoryVariables> loadMemoryVariables([
+    final MemoryInputValues values = const {},
+  ]) async {
+    final messages = <ChatMessage>[];
+    if (_buffer.isNotEmpty) {
+      final msg =
+          summaryMessageBuilder?.call(_buffer) ?? ChatMessage.system(_buffer);
+      messages.add(msg);
+    }
+
+    if (returnMessages) {
+      return {memoryKey: messages};
+    }
+    return {
+      memoryKey: messages.toBufferString(
+        systemPrefix: systemPrefix,
+        humanPrefix: humanPrefix,
+        aiPrefix: aiPrefix,
+        functionPrefix: functionPrefix,
+      ),
+    };
+  }
+
+  @override
+  Future<void> saveContext({
+    required final MemoryInputValues inputValues,
+    required final MemoryOutputValues outputValues,
+  }) async {
+    await super.saveContext(
+      inputValues: inputValues,
+      outputValues: outputValues,
+    );
+    final messages = await chatHistory.getChatMessages();
+    _buffer = await _summarize(
+      messages.sublist(messages.length - 2),
+      _buffer,
+    );
+  }
+
+  Future<String> _summarize(
+    final List<ChatMessage> messages,
+    final String currentSummary,
+  ) async {
+    final input = messages.toBufferString(
+      systemPrefix: systemPrefix,
+      humanPrefix: humanPrefix,
+      aiPrefix: aiPrefix,
+      functionPrefix: functionPrefix,
+    );
+    return LLMChain(llm: llm, prompt: summaryPromptTemplate).run({
+      'new_lines': input,
+      'summary': currentSummary,
+    });
+  }
+
+  @override
+  Future<void> clear() async {
+    await super.clear();
+    _buffer = '';
+  }
+}
---
+++ packages/langchain/test/memory/summary_test.dart
@@ -0,0 +1,224 @@
+import 'package:langchain/src/memory/memory.dart';
+import 'package:langchain/src/memory/summary.dart';
+import 'package:langchain/src/model_io/chat_models/chat_models.dart';
+import 'package:langchain/src/model_io/llms/fake.dart';
+import 'package:langchain/src/model_io/prompts/prompts.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('ConversationSummaryMemory tests', () {
+    test('Test summary memory', () async {
+      final model = FakeHandlerLLM(
+        handler: (final prompt, final options, final callCount) {
+          switch (callCount) {
+            case 1:
+              expect(
+                prompt,
+                'Progressively summarize the lines of conversation provided, adding onto the previous summary returning a new summary.\n\n'
+                'EXAMPLE\n'
+                'Current summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good.\n\n'
+                'New lines of conversation:\n'
+                'Human: Why do you think artificial intelligence is a force for good?\n'
+                'AI: Because artificial intelligence will help humans reach their full potential.\n\n'
+                'New summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good because it will help humans reach their full potential.\n'
+                'END OF EXAMPLE\n\n'
+                'Current summary:\n\n'
+                '\n'
+                'New lines of conversation:\n'
+                'Human: bar\nAI: foo\n\n'
+                'New summary:',
+              );
+              return 'Human said foo';
+            default:
+              throw TestFailure('Unexpected call count: $callCount');
+          }
+        },
+      );
+      final memory = ConversationSummaryMemory(llm: model);
+      final result1 = await memory.loadMemoryVariables();
+      expect(result1, {BaseMemory.defaultMemoryKey: ''});
+
+      await memory.saveContext(
+        inputValues: {'foo': 'bar'},
+        outputValues: {'bar': 'foo'},
+      );
+      final result2 = await memory.loadMemoryVariables();
+      expect(result2, {BaseMemory.defaultMemoryKey: 'System: Human said foo'});
+    });
+
+    test('Test summary memory return system messages', () async {
+      final model = FakeHandlerLLM(
+        handler: (final prompt, final options, final callCount) {
+          switch (callCount) {
+            case 1:
+              expect(
+                prompt,
+                'Progressively summarize the lines of conversation provided, adding onto the previous summary returning a new summary.\n\n'
+                'EXAMPLE\n'
+                'Current summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good.\n\n'
+                'New lines of conversation:\n'
+                'Human: Why do you think artificial intelligence is a force for good?\n'
+                'AI: Because artificial intelligence will help humans reach their full potential.\n\n'
+                'New summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good because it will help humans reach their full potential.\n'
+                'END OF EXAMPLE\n\n'
+                'Current summary:\n\n'
+                '\n'
+                'New lines of conversation:\n'
+                'Human: bar\nAI: foo\n\n'
+                'New summary:',
+              );
+              return 'Human said foo';
+            case 2:
+              expect(
+                prompt,
+                'Progressively summarize the lines of conversation provided, adding onto the previous summary returning a new summary.\n\n'
+                'EXAMPLE\n'
+                'Current summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good.\n\n'
+                'New lines of conversation:\n'
+                'Human: Why do you think artificial intelligence is a force for good?\n'
+                'AI: Because artificial intelligence will help humans reach their full potential.\n\n'
+                'New summary:\n'
+                'The human asks what the AI thinks of artificial intelligence. The AI thinks artificial intelligence is a force for good because it will help humans reach their full potential.\n'
+                'END OF EXAMPLE\n\n'
+                'Current summary:\nHuman said foo\n'
+                '\n'
+                'New lines of conversation:\n'
+                'Human: bar1\nAI: foo1\n\n'
+                'New summary:',
+              );
+              return 'Human said bar';
+            default:
+              throw TestFailure('Unexpected call count: $callCount');
+          }
+        },
+      );
+      final memory =
+          ConversationSummaryMemory(llm: model, returnMessages: true);
+      final result1 = await memory.loadMemoryVariables();
+      expect(result1, {BaseMemory.defaultMemoryKey: <ChatMessage>[]});
+
+      await memory.saveContext(
+        inputValues: {'foo': 'bar'},
+        outputValues: {'bar': 'foo'},
+      );
+      final expectedResult = [ChatMessage.system('Human said foo')];
+      final result2 = await memory.loadMemoryVariables();
+      expect(result2, {BaseMemory.defaultMemoryKey: expectedResult});
+
+      await memory.saveContext(
+        inputValues: {'foo': 'bar1'},
+        outputValues: {'bar': 'foo1'},
+      );
+
+      final expectedResult2 = [ChatMessage.system('Human said bar')];
+      final result3 = await memory.loadMemoryVariables();
+      expect(result3, {BaseMemory.defaultMemoryKey: expectedResult2});
+    });
+
+    test('Test buffer memory with pre-loaded history', () async {
+      const prompt = PromptTemplate(
+        inputVariables: {'summary', 'new_lines'},
+        template: 'Please summary {summary} with {new_lines}',
+      );
+      final model = FakeHandlerLLM(
+        handler: (final prompt, final options, final callCount) {
+          switch (callCount) {
+            case 1:
+              expect(
+                prompt,
+                "Please summary  with Human: My name's Jonas\nAI: Nice to meet you, Jonas!",
+              );
+              return 'Human said foo';
+            default:
+              throw TestFailure('Unexpected call count: $callCount');
+          }
+        },
+      );
+      final pastMessages = [
+        ChatMessage.human("My name's Jonas"),
+        ChatMessage.ai('Nice to meet you, Jonas!'),
+      ];
+      final memory = await ConversationSummaryMemory.fromMessages(
+        llm: model,
+        chatHistory: ChatMessageHistory(messages: pastMessages),
+        summaryPromptTemplate: prompt,
+      );
+      final result = await memory.loadMemoryVariables();
+      expect(result, {BaseMemory.defaultMemoryKey: 'System: Human said foo'});
+    });
+
+    test('Test clear memory', () async {
+      const prompt = PromptTemplate(
+        inputVariables: {'summary', 'new_lines'},
+        template: 'Please summary {summary} with {new_lines}',
+      );
+      final model = FakeHandlerLLM(
+        handler: (final prompt, final options, final callCount) {
+          switch (callCount) {
+            case 1:
+              expect(
+                prompt,
+                'Please summary Human said bar with Human: bar\nAI: foo',
+              );
+              return 'Human said foo';
+            default:
+              throw TestFailure('Unexpected call count: $callCount');
+          }
+        },
+      );
+      final memory = ConversationSummaryMemory(
+        llm: model,
+        summaryPromptTemplate: prompt,
+        initialSummary: 'Human said bar',
+      );
+      await memory.saveContext(
+        inputValues: {'foo': 'bar'},
+        outputValues: {'bar': 'foo'},
+      );
+      const expectedString = 'System: Human said foo';
+      final result1 = await memory.loadMemoryVariables();
+      expect(result1, {BaseMemory.defaultMemoryKey: expectedString});
+
+      await memory.clear();
+      final result2 = await memory.loadMemoryVariables();
+      expect(result2, {BaseMemory.defaultMemoryKey: ''});
+    });
+
+    test('Test summaryMessageBuilder', () async {
+      const prompt = PromptTemplate(
+        inputVariables: {'summary', 'new_lines'},
+        template: 'Please summary {summary} with {new_lines}',
+      );
+      final model = FakeHandlerLLM(
+        handler: (final prompt, final options, final callCount) {
+          switch (callCount) {
+            case 1:
+              expect(
+                prompt,
+                "Please summary  with Human: My name's Jonas\nAI: Nice to meet you, Jonas!",
+              );
+              return 'Human said foo';
+            default:
+              throw TestFailure('Unexpected call count: $callCount');
+          }
+        },
+      );
+      final memory = ConversationSummaryMemory(
+        llm: model,
+        summaryPromptTemplate: prompt,
+        summaryMessageBuilder: ChatMessage.ai,
+      );
+      await memory.saveContext(
+        inputValues: {'foo': "My name's Jonas"},
+        outputValues: {'bar': 'Nice to meet you, Jonas!'},
+      );
+      final result = await memory.loadMemoryVariables();
+      expect(result, {BaseMemory.defaultMemoryKey: 'AI: Human said foo'});
+    });
+  });
+}
