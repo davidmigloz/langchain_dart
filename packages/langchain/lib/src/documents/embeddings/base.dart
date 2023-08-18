85bb31918308f7a956afd0f991a78cf65e6dcd8d
David Miguel Lozano <me@davidmiguel.com>
2023-08-19 00:18:23 +0200

    feat(storage): Add support for EncoderBackedStore (#129)

--- packages/langchain/lib/src/documents/embeddings/base.dart
+++ packages/langchain/lib/src/documents/embeddings/base.dart
@@ -6,7 +6,7 @@ abstract interface class Embeddings {
   const Embeddings();
 
   /// Embed search docs.
-  Future<List<List<double>>> embedDocuments(final List<String> documents);
+  Future<List<List<double>>> embedDocuments(final List<String> texts);
 
   /// Embed query text.
   Future<List<double>> embedQuery(final String query);
