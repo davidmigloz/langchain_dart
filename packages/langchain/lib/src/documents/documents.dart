d9d7268ddcd9e346f67e1278127e25ee467ea99c
David Miguel Lozano <me@davidmiguel.com>
2023-08-18 18:49:43 +0200

    feat(doc-stores): Add support for InMemoryDocStore (#127)

--- packages/langchain/lib/src/documents/documents.dart
+++ packages/langchain/lib/src/documents/documents.dart
@@ -2,5 +2,6 @@ export 'embeddings/embeddings.dart';
 export 'loaders/loaders.dart';
 export 'models/models.dart';
 export 'retrievers/retrievers.dart';
+export 'stores/stores.dart';
 export 'transformers/transformers.dart';
 export 'vector_stores/vector_stores.dart';
