66c8e64410d1dbf8b75e5734cb0cbb0e43dc0615
David Miguel <me@davidmiguel.com>
2023-09-16 16:12:14 +0200

    feat(output-parsers): Add support for JsonOutputFunctionsParser (#165)

--- packages/langchain/lib/src/model_io/output_parsers/functions.dart
+++ packages/langchain/lib/src/model_io/output_parsers/functions.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import '../../core/core.dart';
 import '../chat_models/models/models.dart';
 import '../language_models/models/models.dart';
 import 'models/models.dart';
@@ -6,8 +9,9 @@ import 'output_parser.dart';
 /// {@template base_output_functions_parser}
 /// A parser that converts the output of a function call into a specified type.
 /// {@endtemplate}
-abstract class BaseOutputFunctionsParser<O extends Object>
-    extends BaseLLMOutputParser<ChatMessage, O> {
+abstract class BaseOutputFunctionsParser<
+    CallOptions extends BaseLangChainOptions,
+    O extends Object> extends BaseLLMOutputParser<ChatMessage, CallOptions, O> {
   /// {@macro base_output_functions_parser}
   const BaseOutputFunctionsParser();
 
@@ -29,18 +33,148 @@ abstract class BaseOutputFunctionsParser<O extends Object>
 }
 
 /// {@template output_functions_parser}
+/// A parser that returns the output of a function as a JSON [String].
+/// {@endtemplate}
+class OutputFunctionsParser<CallOptions extends BaseLangChainOptions>
+    extends BaseOutputFunctionsParser<CallOptions, String> {
+  /// {@macro output_functions_parser}
+  const OutputFunctionsParser({
+    this.argsOnly = true,
+  });
+
+  /// Whether to return only the arguments of the function call.
+  final bool argsOnly;
+
+  @override
+  Future<String> parseFunctionCall(
+    final AIChatMessageFunctionCall functionCall,
+  ) async {
+    final map = argsOnly ? functionCall.arguments : functionCall.toMap();
+    return json.encode(map);
+  }
+}
+
+/// {@template json_output_functions_parser}
 /// A parser that converts the output of a function call into a [Map] that
 /// contains the arguments of the function call.
+///
+/// Example:
+/// ```dart
+/// final openaiApiKey = Platform.environment['OPENAI_API_KEY'];
+/// final model = ChatOpenAI(apiKey: openaiApiKey);
+///
+/// final promptTemplate = ChatPromptTemplate.fromTemplate(
+///   'Tell me a joke about {foo}',
+/// );
+///
+/// const function = ChatFunction(
+///   name: 'joke',
+///   description: 'A joke',
+///   parameters: {
+///     'type': 'object',
+///     'properties': {
+///       'setup': {
+///         'type': 'string',
+///         'description': 'The setup for the joke',
+///       },
+///       'punchline': {
+///         'type': 'string',
+///         'description': 'The punchline for the joke',
+///       },
+///     },
+///     'required': ['setup', 'punchline'],
+///   },
+/// );
+///
+/// final chain = promptTemplate |
+///     model.bind(
+///       ChatOpenAIOptions(
+///         functions: const [function],
+///         functionCall: ChatFunctionCall.forced(functionName: function.name),
+///       ),
+///     ) |
+///     const JsonOutputFunctionsParser();
+///
+/// final res = await chain.invoke({'foo': 'bears'});
+/// print(res);
+/// // {
+/// //   setup: Why don't bears wear shoes?,
+/// //   punchline: Because they have bear feet!
+/// // }
+/// ```
 /// {@endtemplate}
-class OutputFunctionsParser
-    extends BaseOutputFunctionsParser<Map<String, dynamic>> {
-  /// {@macro output_functions_parser}
-  const OutputFunctionsParser();
+class JsonOutputFunctionsParser<CallOptions extends BaseLangChainOptions>
+    extends BaseOutputFunctionsParser<CallOptions, Map<String, dynamic>> {
+  /// {@macro json_output_functions_parser}
+  const JsonOutputFunctionsParser();
 
   @override
   Future<Map<String, dynamic>> parseFunctionCall(
     final AIChatMessageFunctionCall functionCall,
   ) async {
     return functionCall.arguments;
+  }
+}
+
+/// {@template json_key_output_functions_parser}
+/// A parser that extracts a specific key from the arguments of a function call.
+///
+/// Example:
+/// ```dart
+/// final openaiApiKey = Platform.environment['OPENAI_API_KEY'];
+/// final model = ChatOpenAI(apiKey: openaiApiKey);
+///
+/// final promptTemplate = ChatPromptTemplate.fromTemplate(
+///   'Tell me a joke about {foo}',
+/// );
+///
+/// const function = ChatFunction(
+///   name: 'joke',
+///   description: 'A joke',
+///   parameters: {
+///     'type': 'object',
+///     'properties': {
+///       'setup': {
+///         'type': 'string',
+///         'description': 'The setup for the joke',
+///       },
+///       'punchline': {
+///         'type': 'string',
+///         'description': 'The punchline for the joke',
+///       },
+///     },
+///     'required': ['setup', 'punchline'],
+///   },
+/// );
+///
+/// final chain = promptTemplate |
+///     model.bind(
+///       ChatOpenAIOptions(
+///         functions: const [function],
+///         functionCall: ChatFunctionCall.forced(functionName: function.name),
+///       ),
+///     ) |
+///     const JsonKeyOutputFunctionsParser(keyName: 'setup');
+///
+/// final res = await chain.invoke({'foo': 'bears'});
+/// print(res);
+/// // Why don't bears wear socks?
+/// ```
+/// {@endtemplate}
+class JsonKeyOutputFunctionsParser<CallOptions extends BaseLangChainOptions,
+    T extends Object> extends BaseOutputFunctionsParser<CallOptions, T> {
+  /// {@macro json_key_output_functions_parser}
+  const JsonKeyOutputFunctionsParser({
+    required this.keyName,
+  });
+
+  /// The key to extract from the arguments of the function call.
+  final String keyName;
+
+  @override
+  Future<T> parseFunctionCall(
+    final AIChatMessageFunctionCall functionCall,
+  ) async {
+    return functionCall.arguments[keyName] as T;
   }
 }
---
+++ packages/langchain/test/model_io/output_parsers/functions_test.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: unused_element
+import 'package:langchain/langchain.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final result = ChatResult(
+    generations: [
+      ChatGeneration(
+        ChatMessage.ai(
+          '',
+          functionCall: const AIChatMessageFunctionCall(
+            name: 'test',
+            arguments: {
+              'foo': 'bar',
+              'bar': 'foo',
+            },
+          ),
+        ),
+      ),
+    ],
+  );
+
+  group('OutputFunctionsParser tests', () {
+    test('OutputFunctionsParser from ChatResult', () async {
+      final res =
+          await const OutputFunctionsParser().parseResult(result.generations);
+      expect(res, '{"foo":"bar","bar":"foo"}');
+    });
+  });
+
+  group('JsonOutputFunctionsParser tests', () {
+    test('JsonOutputFunctionsParser from ChatResult', () async {
+      final res = await const JsonOutputFunctionsParser()
+          .parseResult(result.generations);
+      expect(res, {'foo': 'bar', 'bar': 'foo'});
+    });
+  });
+
+  group('JsonKeyOutputFunctionsParser tests', () {
+    test('JsonKeyOutputFunctionsParser from ChatResult', () async {
+      final res = await const JsonKeyOutputFunctionsParser(keyName: 'foo')
+          .parseResult(result.generations);
+      expect(res, 'bar');
+    });
+  });
+}
