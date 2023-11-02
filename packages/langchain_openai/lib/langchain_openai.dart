273cfa2e84421161c979ddc41168d1b38981ca04
David Miguel Lozano <me@davidmiguel.com>
2023-11-02 23:13:56 +0100

    refactor: Remove dependency on dart_openai (#186)

--- packages/langchain_openai/lib/langchain_openai.dart
+++ packages/langchain_openai/lib/langchain_openai.dart
@@ -4,6 +4,5 @@ library;
 export 'src/agents/agents.dart';
 export 'src/chains/chains.dart';
 export 'src/chat_models/chat_models.dart';
-export 'src/client/client.dart';
 export 'src/embeddings/embeddings.dart';
 export 'src/llms/llms.dart';
