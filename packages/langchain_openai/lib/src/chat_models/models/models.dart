a3c6e2ce40d01de27f683659115cf08c0d01e8d1
David Miguel <me@davidmiguel.com>
2024-01-26 22:06:18 +0100

    feat: Add support for shortening embeddings in OpenAIEmbeddings
    https://openai.com/blog/new-embedding-models-and-api-updates#native-support-for-shortening-embeddings

--- packages/langchain_openai/lib/src/chat_models/models/models.dart
+++ packages/langchain_openai/lib/src/chat_models/models/models.dart
@@ -24,7 +24,26 @@ class ChatOpenAIOptions extends ChatModelOptions {
 
   /// ID of the model to use (e.g. 'gpt-3.5-turbo').
   ///
-  /// See https://platform.openai.com/docs/api-reference/chat/create#chat-create-model
+  /// Available models:
+  /// - `gpt-4`
+  /// - `gpt-4-0314`
+  /// - `gpt-4-0613`
+  /// - `gpt-4-32k`
+  /// - `gpt-4-32k-0314`
+  /// - `gpt-4-32k-0613`
+  /// - `gpt-4-turbo-preview`
+  /// - `gpt-4-1106-preview`
+  /// - `gpt-4-0125-preview`
+  /// - `gpt-4-vision-preview`
+  /// - `gpt-3.5-turbo`
+  /// - `gpt-3.5-turbo-16k`
+  /// - `gpt-3.5-turbo-0301`
+  /// - `gpt-3.5-turbo-0613`
+  /// - `gpt-3.5-turbo-1106`
+  /// - `gpt-3.5-turbo-16k-0613`
+  ///
+  /// Mind that the list may be outdated.
+  /// See https://platform.openai.com/docs/models for the latest list.
   final String? model;
 
   /// Number between -2.0 and 2.0. Positive values penalize new tokens based on
