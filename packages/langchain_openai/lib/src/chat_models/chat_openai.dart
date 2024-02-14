4744648cdf02828b9182ebd34ba3d7db5313786e
David Miguel Lozano <me@davidmiguel.com>
2024-02-14 21:31:50 +0100

    feat: Allow to specify OpenAI custom instance (#327)

--- packages/langchain_openai/lib/src/chat_models/chat_openai.dart
+++ packages/langchain_openai/lib/src/chat_models/chat_openai.dart
@@ -301,6 +301,7 @@ class ChatOpenAI extends BaseChatModel<ChatOpenAIOptions> {
       functionCall: functionCall,
       frequencyPenalty:
           options?.frequencyPenalty ?? defaultOptions.frequencyPenalty,
+      instanceId: options?.instanceId ?? defaultOptions.instanceId,
       logitBias: options?.logitBias ?? defaultOptions.logitBias,
       maxTokens: options?.maxTokens ?? defaultOptions.maxTokens,
       n: options?.n ?? defaultOptions.n,
