a30d10980c705387927402ed16e0aaca93ed7773
Kenneth Knudsen <ksk@blackbird.online>
2024-05-04 15:45:16 +0200

    Create example

--- packages/langchain_google/example/firestore_memory.dart
+++ packages/langchain_google/example/firestore_memory.dart
@@ -1,19 +1,80 @@
+import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:langchain_google/langchain_google.dart';
+import 'package:langchain/langchain.dart';
 
 Future<void> main() async {
-  // final db = FakeFirebaseFirestore();
+  await _example1();
+}
 
-  // await db.collection('Users').doc('a').collection('Langchain').add({
-  //   'username': 'Bob',
-  // });
+Future<void> _example1() async {
+  final db = FakeFirebaseFirestore();
+  const String userId = '1234';
 
-  // final FirestoreChatMessageHistory history = FirestoreChatMessageHistory(
-  //   db.collection('Users').doc('a').collection('Langchain'),
-  // );
+  final FirestoreChatMessageHistory history = FirestoreChatMessageHistory(
+    collections: 'Langchain',
+    options: db.app.options,
+    userId: userId,
+  );
+  //Very important to initialize firestore
+  await history.ensureFirestore();
 
-  // await history.addHumanChatMessage('hi!');
-  // await history.addAIChatMessage('whats up?');
+  await history.addHumanChatMessage('hi!');
+  await history.addAIChatMessage('whats up?');
 
-  // print(await history.getChatMessages());
-  // FirestoreChatMessageHistory his = FirestoreChatMessageHistory(collections: 'Users/', options: options)
+  print(await history.getChatMessages());
+}
+
+//Nested firestore collection
+Future<void> _example2() async {
+  final db = FakeFirebaseFirestore();
+  const String userId = '1234';
+
+  final FirestoreChatMessageHistory history = FirestoreChatMessageHistory(
+    collections: 'Users/$userId/Messages',
+    options: db.app.options,
+    userId: userId,
+  );
+
+  //Very important to initialize firestore
+  await history.ensureFirestore();
+
+  await history.addHumanChatMessage('hi!');
+  await history.addAIChatMessage('whats up?');
+
+  print(await history.getChatMessages());
+  // [HumanChatMessage(content='hi!', example=false),
+  //  AIMessage(content='whats up?', example=false)]
+}
+
+//Nested firestore collection
+Future<void> _example3() async {
+  final db = FakeFirebaseFirestore();
+  const String userId = '1234';
+
+  //Create firestore history to give to ConversationBufferMemory
+  final FirestoreChatMessageHistory history = FirestoreChatMessageHistory(
+    collections: 'Users/$userId/Messages',
+    options: db.app.options,
+    userId: userId,
+  );
+
+  //Very important to initialize firestore
+  await history.ensureFirestore();
+
+  //Create ConversationBufferMemory instance to give to Chain
+  final ConversationBufferMemory memory =
+      ConversationBufferMemory(chatHistory: history);
+
+  //Create llm instanse
+  final llm = FakeLLM(responses: ['Hey there what can I help you with?']);
+
+  //Create chain with memory
+  final ConversationChain chain = ConversationChain(llm: llm, memory: memory);
+
+  //Call llm
+  await chain.run('Hi there!');
+
+  print(await history.getChatMessages());
+  // [HumanChatMessage(content='Hi there!', example=false),
+  //  AIMessage(content='Hey there what can I help you with?', example=false)]
 }
