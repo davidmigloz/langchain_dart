80f0a454745978a33e769caadc0f383edbfc0083
Douglas Bett <bettdalpha@gmail.com>
2024-08-09 00:42:35 +0300

    (fix): Warn on broken markdown links

--- docs_v2/docusaurus.config.js
+++ docs_v2/docusaurus.config.js
@@ -23,8 +23,8 @@ const config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
