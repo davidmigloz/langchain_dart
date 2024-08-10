53aaa433cbe297ae08eede1bbc5244f64e34f510
David Miguel Lozano <me@davidmiguel.com>
2024-08-10 18:38:43 +0200

    feat: Add gpt-4o-2024-08-06 to model catalog in openai_dart (#522)

--- packages/openai_dart/lib/src/generated/schema/assistant_tools.dart
+++ packages/openai_dart/lib/src/generated/schema/assistant_tools.dart
@@ -80,7 +80,7 @@ class AssistantToolsFileSearchFileSearch
 
   /// Factory constructor for AssistantToolsFileSearchFileSearch
   const factory AssistantToolsFileSearchFileSearch({
-    /// The maximum number of results the file search tool should output. The default is 20 for gpt-4* models
+    /// The maximum number of results the file search tool should output. The default is 20 for `gpt-4*` models
     /// and 5 for gpt-3.5-turbo. This number should be between 1 and 50 inclusive.
     ///
     /// Note that the file search tool may output fewer than `max_num_results` results. See the [file search
