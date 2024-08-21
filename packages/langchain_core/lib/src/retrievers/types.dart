2156fa3f74738b7197de882660821f96859b7fc6
David Miguel Lozano <me@davidmiguel.com>
2024-08-21 09:24:54 +0200

    feat: Add copyWith method to all RunnableOptions subclasses (#531)

--- packages/langchain_core/lib/src/retrievers/types.dart
+++ packages/langchain_core/lib/src/retrievers/types.dart
@@ -21,10 +21,22 @@ class VectorStoreRetrieverOptions extends RetrieverOptions {
   /// {@macro vector_store_retriever_options}
   const VectorStoreRetrieverOptions({
     this.searchType = const VectorStoreSimilaritySearch(),
+    super.concurrencyLimit,
   });
 
   /// The type of search to perform, either:
   /// - [VectorStoreSearchType.similarity] (default)
   /// - [VectorStoreSearchType.mmr]
   final VectorStoreSearchType searchType;
+
+  @override
+  VectorStoreRetrieverOptions copyWith({
+    final VectorStoreSearchType? searchType,
+    final int? concurrencyLimit,
+  }) {
+    return VectorStoreRetrieverOptions(
+      searchType: searchType ?? this.searchType,
+      concurrencyLimit: concurrencyLimit ?? this.concurrencyLimit,
+    );
+  }
 }
