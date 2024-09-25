71143ff2df4181aeed5fccaffa1f55d57cf374b4
David Miguel Lozano <me@davidmiguel.com>
2024-09-25 22:06:55 +0200

    feat: Add support for deleteWhere in ObjectBoxVectorStore (#552)

--- packages/langchain_community/lib/src/vector_stores/objectbox/base_objectbox.dart
+++ packages/langchain_community/lib/src/vector_stores/objectbox/base_objectbox.dart
@@ -14,8 +14,101 @@ import 'package:uuid/uuid.dart';
 /// {@template base_object_box_vector_store}
 /// Base class for ObjectBox vector store.
 ///
-/// Use this class if you need more control over the ObjectBox store.
-/// Otherwise, use [ObjectBoxVectorStore] which is a pre-configured version.
+/// The [ObjectBoxVectorStore] class is a pre-configured version of this class,
+/// but it can only be used if you don't use ObjectBox for anything else.
+///
+/// If you need more control over the ObjectBox store, use this class instead.
+/// For example, if you are using ObjectBox to store other entities, or if you
+/// need to customize the Document entity class.
+///
+/// Here is an example of how to use this class:
+///
+/// First, you can define our own Document entity class instead of using the
+/// one provided by the [ObjectBoxVectorStore]. In this way, you can customize
+/// the entity to your needs. You will need to define the mapping logic between
+/// the entity and the LangChain [Document] model.
+///
+/// ```dart
+/// @Entity()
+/// class MyDocumentEntity {
+///   MyDocumentEntity({
+///     required this.id,
+///     required this.content,
+///     required this.metadata,
+///     required this.embedding,
+///   });
+///
+///   @Id()
+///   int internalId = 0;
+///
+///   @Unique(onConflict: ConflictStrategy.replace)
+///   String id;
+///
+///   String content;
+///
+///   String metadata;
+///
+///   @HnswIndex(
+///     dimensions: 768,
+///     distanceType: VectorDistanceType.cosine,
+///   )
+///   @Property(type: PropertyType.floatVector)
+///   List<double> embedding;
+///
+///   factory MyDocumentEntity.fromModel(
+///     Document doc, List<double> embedding,
+///   ) => MyDocumentEntity(
+///         id: doc.id ?? '',
+///         content: doc.pageContent,
+///         metadata: jsonEncode(doc.metadata),
+///         embedding: embedding,
+///       );
+///
+///   Document toModel() => Document(
+///         id: id,
+///         pageContent: content,
+///         metadata: jsonDecode(metadata),
+///       );
+/// }
+/// ```
+///
+/// After defining the entity class, you will need to run the ObjectBox
+/// generator:
+///
+/// ```sh
+/// dart run build_runner build --delete-conflicting-outputs
+/// ```
+///
+/// Then, you just need to create your custom vector store class that
+/// extends [BaseObjectBoxVectorStore] and wire everything up:
+///
+/// ```dart
+/// class MyCustomVectorStore extends BaseObjectBoxVectorStore<MyDocumentEntity> {
+///   MyCustomVectorStore({
+///     required super.embeddings,
+///     required Store store,
+///   }) : super(
+///           box: store.box<MyDocumentEntity>(),
+///           createEntity: (
+///             String id,
+///             String content,
+///             String metadata,
+///             List<double> embedding,
+///           ) =>
+///               MyDocumentEntity(
+///             id: id,
+///             content: content,
+///             metadata: metadata,
+///             embedding: embedding,
+///           ),
+///           createDocument: (MyDocumentEntity docDto) => docDto.toModel(),
+///           getIdProperty: () => MyDocumentEntity_.id,
+///           getEmbeddingProperty: () => MyDocumentEntity_.embedding,
+///         );
+/// }
+/// ```
+///
+/// Now you can use the [MyCustomVectorStore] class to store and search documents.
 /// {@endtemplate}
 class BaseObjectBoxVectorStore<T> extends VectorStore {
   /// {@macro base_object_box_vector_store}
@@ -87,8 +180,15 @@ class BaseObjectBoxVectorStore<T> extends VectorStore {
   }
 
   @override
-  Future<void> delete({required final List<String> ids}) async {
-    _box.query(_getIdProperty().oneOf(ids)).build().remove();
+  Future<void> delete({required final List<String> ids}) {
+    return _box.query(_getIdProperty().oneOf(ids)).build().removeAsync();
+  }
+
+  /// Delete by condition.
+  ///
+  /// - [condition] is the condition to delete by.
+  Future<void> deleteWhere(final Condition<T> condition) {
+    return _box.query(condition).build().removeAsync();
   }
 
   @override
