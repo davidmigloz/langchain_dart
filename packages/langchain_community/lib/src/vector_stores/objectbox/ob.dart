4fc0e7a5766ca2c56a74cfa1a366499a8ff43b9e
David Miguel Lozano <me@davidmiguel.com>
2024-09-25 22:46:03 +0200

    refactor: Add stubs for ObjectBox on web platform (#553)

---
+++ packages/langchain_community/lib/src/vector_stores/objectbox/ob.dart
@@ -0,0 +1,7 @@
+export 'ob_io.dart' if (dart.library.js_interop) 'ob_stub.dart'
+    show
+        BaseObjectBoxVectorStore,
+        ObjectBoxDocument,
+        ObjectBoxDocumentProps,
+        ObjectBoxSimilaritySearch,
+        ObjectBoxVectorStore;
--- packages/langchain_community/lib/src/vector_stores/vector_stores.dart
+++ packages/langchain_community/lib/src/vector_stores/vector_stores.dart
@@ -1,4 +1,1 @@
-export 'objectbox/base_objectbox.dart' show BaseObjectBoxVectorStore;
-export 'objectbox/objectbox.dart'
-    show ObjectBoxDocument, ObjectBoxDocumentProps, ObjectBoxVectorStore;
-export 'objectbox/types.dart' show ObjectBoxSimilaritySearch;
+export 'objectbox/ob.dart';
