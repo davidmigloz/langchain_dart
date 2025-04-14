6b1d501bf4a1110ed5f73f8c64d4cc100186d39b
mbfakourii <mbfakourii@gmail.com>
2025-04-14 11:09:37 +0330

    feat: Add support for custom headers in WebSocket connections

--- packages/openai_realtime_dart/lib/src/web_socket/web_socket_io.dart
+++ packages/openai_realtime_dart/lib/src/web_socket/web_socket_io.dart
@@ -2,12 +2,17 @@ import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 /// Creates a new WebSocket connection using the `dart:io` implementation.
-WebSocketChannel connectWebSocket(Uri uri, String? apiKey) {
+WebSocketChannel connectWebSocket(
+  Uri uri,
+  String? apiKey,
+  Map<String, String>? headers,
+) {
   return IOWebSocketChannel.connect(
     uri,
     headers: {
       if (apiKey != null) 'Authorization': 'Bearer $apiKey',
       'OpenAI-Beta': 'realtime=v1',
+      ...?headers,
     },
   );
 }
