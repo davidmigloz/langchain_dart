d69b5fde4b07b7e4f9e372a820ada9706141d8dd
Sheldon Robinson <sheldon.robinson@gmail.com>
2025-12-14 08:40:15 -0500

    Update to handle MCP tools

--- packages/langchain/lib/src/agents/executor.dart
+++ packages/langchain/lib/src/agents/executor.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:langchain_core/agents.dart';
 import 'package:langchain_core/chains.dart';
 import 'package:langchain_core/output_parsers.dart';
@@ -199,7 +201,8 @@ class AgentExecutor extends BaseChain {
       String observation;
       if (tool != null) {
         final toolInput = tool.getInputFromJson(agentAction.toolInput);
-        observation = (await tool.invoke(toolInput)).toString();
+        final toolOutput = await tool.invoke(toolInput);
+        observation = toolOutput is String ? toolOutput : jsonEncode(toolOutput);
       } else {
         observation =
             '${agentAction.tool} is not a valid tool, try another one.';
